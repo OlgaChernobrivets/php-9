2e861397cf2b5417e396393c485565e781f4b048
OlgaChernobrivets <gmco555@gmail.com>
2017-02-10 19:14:47 +0500

    Update script.php

--- script.php
+++ script.php
@@ -1,20 +1,23 @@
-<?php 
-error_reporting(E_ALL);
-ini_set ('display_errors', 1);
+	<?php 
+	error_reporting(E_ALL);
+	ini_set ('display_errors', 1);
 
-$x = (int) $_GET['x'];
-$a = 1;
-$b = 1;
-echo 'Вы ввели число: '.$x;
-echo '</br>';
-while ($a < $x) {
-$c = $a;
-$a = $a + $b;
-$b = $c;
-}
-if ($a > $x) {
-	echo 'Данное число не входит в ряд Фибонначи';
-}
-if ($a === $x) {
-	echo 'Данное число входит в ряд Фибонначи';
-}
+	$x = (int) $_GET['x'];
+	$first_variable = 1;
+	$second_variable = 1;
+	echo 'Вы ввели число: '.$x;
+	echo '</br>';
+	while ($first_variable < $x) 
+	{
+		$third_variable = $first_variable;
+		$first_variable = $first_variable + $second_variable;
+		$second_variable = $third_variable;
+	}
+	if ($first_variable > $x)
+	{
+		echo 'Данное число не входит в ряд Фибонначи';
+	}
+	if ($first_variable === $x)
+	{
+		echo 'Данное число входит в ряд Фибонначи';
+	}
